690a53bb54af17c5f5e577b10fce827bdce43dbc
ericsyz <52880789+ericsyz@users.noreply.github.com>
2022-10-22 17:59:20 -0400

    Revert "Evan"

--- keylime/src/components/exportPages.js
+++ keylime/src/components/exportPages.js
@@ -2,6 +2,5 @@ export { Account } from "./Account/Account";
 export { NavigationBar } from './NavigationBar';
 export { Landing } from '../pages/Landing';
 export { Login } from './Account/Login';
-export { Application } from './Application';
 export { Footer } from './Footer/Footer';
 export { Profile } from './Profile';
