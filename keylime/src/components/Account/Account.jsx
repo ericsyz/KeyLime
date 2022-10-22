1be4c74a80d1b7d113f68049e966ac3c82bc0307
ezhou66 <ericzhou3@gmail.com>
2022-10-22 12:58:05 -0400

    minor changes

--- keylime/src/components/Account/Account.jsx
+++ keylime/src/components/Account/Account.jsx
@@ -1,13 +1,52 @@
-import React, { Component } from "react";
-import { ChakraProvider } from '@chakra-ui/react'
+import { React, useState } from "react";
+import { ChakraProvider, Input, Container, extendTheme, Center, FormControl, FormLabel, Button} from '@chakra-ui/react'
+import { loginWithEmailAndPassword } from "../../firebase";
+
+const theme = extendTheme({
+    textStyles: {
+      h1: {
+        // you can also use responsive styles
+        fontSize: ['48px', '72px'],
+        fontWeight: 'bold',
+        lineHeight: '110%',
+        letterSpacing: '-2%',
+      },
+      h2: {
+        fontSize: ['36px', '48px'],
+        fontWeight: 'semibold',
+        lineHeight: '110%',
+        letterSpacing: '-1%',
+      },
+    },
+  })
+  
+  
 
 export function Account(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <ChakraProvider>
-            <div>
-            <h1>Welcome!</h1>
-            <p>WIP</p>
-            </div>
+            <Container marginTop={40} color = 'green'>
+                <Center textStyle = 'h2'>Welcome!</Center>
+                <FormControl isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input 
+                        type='email' 
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <FormLabel>Password</FormLabel>
+                    <Input type='password' />
+                    <Button colorScheme='teal'
+                        onClick={() => {
+                            
+                        }}
+                    >Submit</Button>
+                </FormControl>
+            </Container>
         </ChakraProvider>
     )
 
