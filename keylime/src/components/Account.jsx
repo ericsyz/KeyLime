38305736a19b82687b94283e97a3b8e572d318d8
ezhou66 <ericzhou3@gmail.com>
2022-10-21 21:45:41 -0400

    add account

---
+++ keylime/src/components/Account.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export function Account(){
+    return (
+        ChakraProvider (
+            <div>
+            <h1>Welcome!</h1>
+            <p>WIP</p>
+            </div>
+        )
+    )
+
+}
