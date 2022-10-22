97ae4f90973568eae90146120ca63134a66ad56f
Evan Zhu <evanzhu888@gmail.com>
2022-10-22 17:33:07 -0400

    Added Location to Form

--- keylime/src/components/Account/Form.js
+++ keylime/src/components/Account/Form.js
@@ -71,30 +71,85 @@ export default function Form() {
               <Divider mt = {4} mb = {2} />
               <Box p = {1}>
                 <Box textAlign="center">
-                    <Heading fontSize = {20}>Emergency Contact</Heading>
+                    <Heading fontSize = {20}>Additional Information</Heading>
                 </Box>
               </Box>
               <FormControl isRequired>
-                <FormLabel fontSize={14}>First name</FormLabel>
-                <Input type="text" placeholder="John" size = "sm"/>
+                <FormLabel fontSize={14}>Address Line 1</FormLabel>
+                <Input type="text" size = "sm"/>
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel fontSize={14}>Address Line 2</FormLabel>
+                <Input type="text" size = "sm"/>
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel fontSize={14}>Address Line 3</FormLabel>
+                <Input type="text" size = "sm"/>
+              </FormControl>
+              <FormControl mt={4} require>
+                <FormLabel fontSize={14}>City</FormLabel>
+                <Input type="text" size = "sm"/>
               </FormControl>
               <FormControl mt={4} isRequired>
-                <FormLabel fontSize={14}>Last Name</FormLabel>
-                <Input type="text" placeholder="Smith"  size = "sm"/>
+                <FormLabel fontSize={14}>State</FormLabel>
+                <Select placeholder='Select'>
+                  <option value="AL">Alabama</option>
+	                <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District Of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </Select>
               </FormControl>
               <FormControl mt={4} isRequired>
-                <FormLabel fontSize={14}>Relationship</FormLabel>
-                <Input type="text" placeholder="Mother"  size = "sm"/>
+                <FormLabel fontSize={14}>Zip Code</FormLabel>
+                <Input type="number" pattern="[0-9]{5}" size = "sm"/>
               </FormControl>
-              <FormControl mt={4} isRequired>
-                <FormLabel fontSize={14}>Email</FormLabel>
-                <Input type="email" placeholder="John@gatech.edu" size = "sm"/>
-              </FormControl>
-              <FormControl mt={4} isRequired>
-                <FormLabel fontSize={14}>Phone Number</FormLabel>
-                <Input type="tel" placeholder="111-111-1111" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" size = "sm"/>
-              </FormControl>
-
               <Button width="text" mt={4} type="submit">
                 Submit
               </Button>
