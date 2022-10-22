2909f95087c8415220265724e85992948d530812
ezhou66 <ericzhou3@gmail.com>
2022-10-22 10:29:51 -0400

    first component

--- keylime/src/App.js
+++ keylime/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Account} from "./components/exportPages";
+import { HashRouter, NavLink, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <main>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Account />} />
+          </Routes>
+        </div>
+      </main>
+    </HashRouter>
   );
 }
 
--- keylime/src/components/Account/Account.jsx
+++ keylime/src/components/Account/Account.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export function Account(){
     return (
-        ChakraProvider (
+        <ChakraProvider>
             <div>
             <h1>Welcome!</h1>
             <p>WIP</p>
             </div>
-        )
+        </ChakraProvider>
     )
 
 }
