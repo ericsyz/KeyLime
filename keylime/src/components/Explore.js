c8992c3e137934d17bc7528863032456af97268b
ezhou66 <ericzhou3@gmail.com>
2022-10-23 02:36:57 -0400

    explore button

--- keylime/src/components/Explore.js
+++ keylime/src/components/Explore.js
@@ -1,5 +1,6 @@
-import { Grid, VStack, Text, WrapItem, Image, Button, Stack, Box, ButtonGroup, Wrap, Center } from '@chakra-ui/react'
+import { Grid, VStack, Text, WrapItem, Image, Button, Stack, Box, ButtonGroup, Wrap, Center, ChakraProvider } from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react';
+import { NavigationBar } from './exportPages';
 
 
 export function Explore() {
@@ -14,17 +15,16 @@ export function Explore() {
         {houseName: 'Chic beachhouse', price: '$1200', urlImage: 'https://www.bhg.com/thmb/a-NwJnw4qLipa1EWsJThQyc7Bik=/1280x1280/smart/filters:no_upscale():focal(899x639:901x641)/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg'}
     ])
     return (
-
-            <div className='house-list'>
-                {houses.map((house, index) => (
-                        
+        <ChakraProvider>
+            <NavigationBar />
+                <div className='house-list'>
+                    {houses.map((house, index) => (
                         <Button variant='ghost' mb={75} mt={75} ml={20} >
-                            
                             <input type="image" src="https://www.bhg.com/thmb/a-NwJnw4qLipa1EWsJThQyc7Bik=/1280x1280/smart/filters:no_upscale():focal(899x639:901x641)/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg" name="submit" width="210" height="150" alt="submit"/>
-                            
                         </Button>
-                    
                 ))}
             </div>
+        </ChakraProvider>
+            
         )
 }
